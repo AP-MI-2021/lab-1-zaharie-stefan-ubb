12ed120ab1a9e59b8e550b4af3dc1280710ea505
Zaharie Stefan <stefan@zaha.tech>
2021-09-28 08:22:16 +0300

    Temp functions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
+  pass
   
   
 '''
@@ -10,6 +11,7 @@ Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
+  pass
   
   
 '''
@@ -17,6 +19,7 @@ Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
+  pass
   
   
 '''
@@ -24,10 +27,12 @@ Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
+  pass
   
   
 def main():
   # interfata de tip consola aici
+  pass
 
 if __name__ == '__main__':
   main()
